cc01f85a8ead970a88282e05a2ba006c7eb42f64
Christian Miller <christian.s.miller-1@ou.edu>
2024-01-24 11:15:47 -0600

    initial commit

---
+++ R/myquad.r
@@ -0,0 +1,12 @@
+#' A Quadratic Function
+#'
+#' @param x A numeric vector
+#'
+#' @return A numeric vector
+#' @export
+#'
+#' @examples
+#' myquad(x = 1:10)
+myquad <- function(x) {
+  x^2 - 5*x + 6
+}
